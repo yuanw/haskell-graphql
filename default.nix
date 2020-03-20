1bbe2b02b2df4d7649337f81b029208544842e11
Yuan Wang <yuan.wang@workiva.com>
2020-03-20 14:23:23 -0600

    trying hook

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
- let
-   nix-pre-commit-hooks = import (builtins.fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/tarball/master");
- in {
-   pre-commit-check = nix-pre-commit-hooks.run {
-     src = ./.;
-     hooks = {
-       elm-format.enable = true;
-       ormolu.enable = true;
-       shellcheck.enable = true;
-     };
-   };
- }
+let
+  nix-pre-commit-hooks = import (builtins.fetchTarball "https://github.com/cachix/pre-commit-hooks.nix/tarball/master");
+in
+{
+  pre-commit-check = nix-pre-commit-hooks.run {
+    src = ./.;
+    hooks = {
+      nixpkgs-fmt.enable = true;
+      hlint.enable = true;
+      cabal-fmt.enable = true;
+    };
+  };
+}
